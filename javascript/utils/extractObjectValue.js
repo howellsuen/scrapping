1caed231e32e93bdb38fab5754ab2f7e3f59f0e7
Howell <howell.suen@gmail.com>
2019-11-14 12:47:20 +0800

    Update

--- javascript/utils/extractObjectValue.js
+++ javascript/utils/extractObjectValue.js
@@ -1,35 +1,39 @@
 const fs = require("fs");
 
-const extractObjectValue = async(fileName, writeFileName) => {
-  try {
-    await fs.readFile(`./storage/${fileName}`, 'utf8', (err, data) => {
-      if (err) return console.log(err)
-      const target = JSON.parse(data)
-      const results = []
-      Object.keys(target).forEach(function (x, i) {
-        Object.keys(target[x]).forEach(function (y, i) {
-          if (typeof target[x][y] === 'object') {
-            Object.keys(target[x][y]).forEach(function (z, i) {
-              if (typeof target[x][y][z] === 'object') {
-                Object.keys(target[x][y][z]).forEach(function (a, i) {
-                  results.push(target[x][y][z][a])
-                })
-              } else {
-                results.push(target[x][y][z])
-              }
-            })
-          } else {
-            results.push(target[x][y])
-          }
-        })
-      })
-      fs.writeFile(`./storage/${writeFileName}`, JSON.stringify(results), 'utf8', function (err) {
-        if (err) return console.log(err)
-      });
-    })
-  } catch (err) {
-    return console.log(err)
-  }
+const extractObjectValue = async (fileName, writeFileName) => {
+	try {
+		await fs.readFile(`./storage/${fileName}`, 'utf8', (err, data) => {
+			if (err) return console.log(err)
+			const target = JSON.parse(data)
+			const results = []
+			Object.keys(target).forEach(function(x, i) {
+				// for not nested object
+				// results.push(target[x])
+
+				// for nested object
+				Object.keys(target[x]).forEach(function(y, i) {
+					if (typeof target[x][y] === 'object') {
+						Object.keys(target[x][y]).forEach(function(z, i) {
+							if (typeof target[x][y][z] === 'object') {
+								Object.keys(target[x][y][z]).forEach(function(a, i) {
+									results.push(target[x][y][z][a])
+								})
+							} else {
+								results.push(target[x][y][z])
+							}
+						})
+					} else {
+						results.push(target[x][y])
+					}
+				})
+			})
+			fs.writeFile(`./storage/${writeFileName}`, JSON.stringify(results), 'utf8', function(err) {
+				if (err) return console.log(err)
+			});
+		})
+	} catch (err) {
+		return console.log(err)
+	}
 }
 
 extractObjectValue('uk.json', 'uk-extracted.csv')
